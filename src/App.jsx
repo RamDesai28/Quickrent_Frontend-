7756bf366fa46cbd6a6eaafbe96cbfa4b5ddd794
bobby <ramdesai9600@gmail.com>
2024-12-15 18:24:28 +0530

    Updated import paths for components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "/components/Navbar";
-import HeroBanner from "/components/HeroBanner";
-import Features from "/components/Features";
-import GadgetListing from "/components/GadgetListing";
-import ContactUs from "/components/ContactUs";
-import Footer from "/components/Footer";
+import Navbar from "./components/Navbar";
+import HeroBanner from "./components/HeroBanner";
+import Features from "./components/Features";
+import GadgetListing from "./components/GadgetListing";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
 import "/styles/global.css";
 import "./A.css"
 
